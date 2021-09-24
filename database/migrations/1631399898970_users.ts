a31c7c6aa6b8d71f37b2bf1f74886962a83b8ccd
Jota <jjunior@protonmail.ch>
2021-09-24 11:27:21 -0300

    fix: set email as unique

--- database/migrations/1631399898970_users.ts
+++ database/migrations/1631399898970_users.ts
@@ -3,10 +3,10 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class UsersSchema extends BaseSchema {
   protected tableName = 'users'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
-      table.string('email', 255).notNullable()
+      table.string('email', 255).notNullable().unique()
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
 
@@ -18,7 +18,7 @@ export default class UsersSchema extends BaseSchema {
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
