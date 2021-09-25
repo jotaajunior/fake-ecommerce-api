c80f8a4bf0ab1bb124f7e992524be80d5b0fd6f7
Jota <jjunior@protonmail.ch>
2021-09-25 00:02:22 -0300

    feat: add search in products

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -1,12 +1,23 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 import Product from 'App/Models/Product'
 
 export default class ProductsController {
   /**
    * Returns a list containing all the products
    */
-  public async index({ response }: HttpContextContract) {
-    const products = await Product.all()
+  public async index({ response, request }: HttpContextContract) {
+    const { query } = request.qs()
+
+    console.log(query)
+
+    const products = await Database.from('products').if(query, (q) => {
+      q.whereRaw(`LOWER(name) like '%${query.toLowerCase()}%'`).orWhereRaw(
+        `LOWER(description) like '%${query.toLowerCase()}%'`
+      )
+    })
+
+    console.log(`%${query}%`)
 
     return response.ok(products)
   }
